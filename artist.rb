08d0cc1b4ade0c5c764ba050cece266adeb90be5
YanC Lin <yonalin1@gmail.com>
2018-03-01 21:54:14 +0000

    automatically backed up by learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,19 @@
 class Artist < ActiveRecord::Base
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  )
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+end
+
 end
